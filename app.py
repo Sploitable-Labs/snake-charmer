93b9639e9dff8bdc4b078e55806b589166ca348b
Autonomoid <37911007+Autonomoid@users.noreply.github.com>
2024-10-29 17:27:02 +0000

    Added page refresh on the first time the ninja challenge is unlocked

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ app.config.update(
 challenges = []
 ninja_challenges = []
 
-ninja_unlock_threshold = 500
+ninja_unlock_threshold = 10
 
 def load_all_challenges():
     global challenges, ninja_challenges
